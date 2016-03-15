d0cd9f046b7e363ad7bbe79d45d83290f9e7f231
almhirte <almhirte@yahoo.com>
2016-03-15 17:01:36 +0100

    Rejected commit: Default

--- app/src/main/java/com/almhirte/honestj/OverviewActivity.java
+++ app/src/main/java/com/almhirte/honestj/OverviewActivity.java
@@ -1,7 +1,9 @@
 package com.almhirte.honestj;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -14,7 +16,7 @@ import android.widget.Toast;
 
 public class OverviewActivity extends AppCompatActivity
 {
-
+    private static String appName = "HonestJ";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -27,6 +29,7 @@ public class OverviewActivity extends AppCompatActivity
     private void initGui()
     {
         View v = findViewById(R.id.tableLayout);
+        Log.d(appName, v.toString());
         if(v instanceof ViewGroup)
         {
             ((ViewGroup)v).removeAllViews();
@@ -45,26 +48,31 @@ public class OverviewActivity extends AppCompatActivity
             String currentRecord = db.loadNextRecord();
             TableLayout table = (TableLayout)v;
 
-            TableRow tr = new TableRow(this);
-            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
-            TextView textview = new TextView(this);
-            textview.setText(currentRecord);
-
-            ImageView imageView = new ImageView(this);
-            imageView.setImageResource(R.mipmap.ic_no_image_rect);
-
-            ImageButton minusBtn = new ImageButton(this);
-            minusBtn.setImageResource(R.mipmap.ic_launcher);
-
-            tr.addView(minusBtn);
-            tr.addView(imageView);
-            tr.addView(textview);
+            TableRow tr = createTableRow(currentRecord);
 
             table.addView(tr);
             i++;
         }
     }
 
+    @NonNull
+    private TableRow createTableRow(String currentRecord) {
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT));
+        TextView textview = new TextView(this);
+        textview.setText(currentRecord);
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.mipmap.ic_no_image_rect);
+
+        ImageButton minusBtn = new ImageButton(this);
+        minusBtn.setImageResource(R.mipmap.ic_launcher);
+
+        tr.addView(minusBtn);
+        tr.addView(imageView);
+        tr.addView(textview);
+        return tr;
+    }
 
 
     public void onClickColumn(View view)
